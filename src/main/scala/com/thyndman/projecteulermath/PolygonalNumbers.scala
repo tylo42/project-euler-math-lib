b28798a41029e83ccea3e37e20dc109f9bdaf2bb
Tyler Hyndman <tyler.hyndman@gmail.com>
2021-03-03 13:19:24 -0600

    Use generic formula for polygonal numbers

--- src/main/scala/com/thyndman/projecteulermath/PolygonalNumbers.scala
+++ src/main/scala/com/thyndman/projecteulermath/PolygonalNumbers.scala
@@ -2,24 +2,27 @@ package com.thyndman.projecteulermath
 
 object PolygonalNumbers {
 
-  def triangle(n: Int): Int = (n * (n+1)) / 2
+  def triangle: Int => Int = generic(3)
   def triangleStream: Stream [Int] = buildStream(triangle)
 
-  def square(n: Int): Int = n * n
+  def square: Int => Int = generic(4)
   def squareStream: Stream [Int] = buildStream(square)
 
-
-  def pentagonal(n: Int): Int = (n * ((3 * n) - 1)) / 2
+  def pentagonal: Int => Int = generic(5)
   def pentagonalStream: Stream [Int] = buildStream(pentagonal)
 
-  def hexagonal(n: Int): Int = n * ((2 * n) - 1)
+  def hexagonal: Int => Int = generic(6)
   def hexagonalStream: Stream [Int] = buildStream(hexagonal)
 
-  def heptagonal(n: Int): Int = (n * ((5 * n) - 3)) / 2
+  def heptagonal: Int => Int = generic(7)
   def heptagonalStream: Stream [Int] = buildStream(heptagonal)
 
-  def octagonal(n: Int): Int = n * ((3 * n) - 2)
+  def octagonal: Int => Int = generic(8)
   def octagonalStream: Stream [Int] = buildStream(octagonal)
+
+  private def generic(sides: Int)(n: Int) = {
+    (sides - 2) * ((n * (n-1))/ 2) + n
+  }
 
   private def buildStream(f: Int => Int) = Stream.from(1).map(f)
 }
